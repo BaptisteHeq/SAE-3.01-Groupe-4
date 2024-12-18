8e6c46f9f8e485b9b24111d87e12168a450a1c2b
Nilecram <marcelin.ray@protonmail.com>
2024-12-18 17:37:33 +0100

    reparation bizarre
    ne voulait plus démarrer sans javafx

--- src/SAE/MainTest.java
+++ src/SAE/MainTest.java
@@ -2,8 +2,16 @@ package SAE;
 
 import java.util.List;
 
-public class MainTest {
+
+
+public class MainTest extends javafx.application.Application {
+
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(javafx.stage.Stage primaryStage) {
         GestionnaireClasses gestionnaire = new GestionnaireClasses();
         ImporteurProjet importeur = new ImporteurProjet(gestionnaire);
         importeur.importerProjet("src/SAE");
--- src/SAE/MainTestImage.java
+++ src/SAE/MainTestImage.java
@@ -2,15 +2,23 @@ package SAE;
 
 import java.io.IOException;
 
-public class MainTestImage {
-    public static void main(String[] args) {
 
+public class MainTestImage extends javafx.application.Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(javafx.stage.Stage primaryStage) {
         GestionnaireClasses gestionnaire = new GestionnaireClasses();
         ImporteurProjet importeur = new ImporteurProjet(gestionnaire);
         importeur.importerProjet("src/SAE");
 
         ExportateurImagePlantUML exportateurImage = new ExportateurImagePlantUML();
-        
-        exportateurImage.exporterImage(gestionnaire, "C:/Users/dieud/OneDrive/univ/2A/diagramme.png");
+
+        exportateurImage.exporterImage(gestionnaire, "zizi.png");
     }
 }
+
+
